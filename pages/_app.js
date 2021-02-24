63d386a633bd9ea42c76779638d0d4c416ae03d3
Joseph Vega <jcvb0206@gmail.com>
2021-02-23 20:48:12 -0500

    Delete deprecated styles and set default colors and font to project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
+import '../styles/_app.css';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const theme = {
+  colors: {
+    white: '#ffffff',
+    grayLighter: '#f6f7f9',
+    grayLight: '#e8eaf0',
+    grayDark: '#8d8d9d',
+    baseMain: '#181b32',
+    black: '#000000',
+  },
+  fonts: {
+    inter: 'Inter',
+  },
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
