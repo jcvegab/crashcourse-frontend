bb768853611549e179552a169f2a99da33ee1f64
Joseph Vega <jcvb0206@gmail.com>
2021-02-24 00:31:49 -0500

    Logo of app redirect to root path

--- components/UI/Logo.js
+++ components/UI/Logo.js
@@ -2,19 +2,20 @@ import styled from 'styled-components';
 import { StyledH1 } from './Title';
 import Link from 'next/link';
 
-const StyledLogo = styled.div`
+const StyledLogo = styled.a`
   display: flex;
   align-items: center;
   gap: 8px;
+  text-decoration: none;
 `;
 
 export default function Logo() {
   return (
-    <StyledLogo>
-      <Link href="/">
+    <Link prefetch href="/" passHref>
+      <StyledLogo>
         <img src="/logo.svg" alt="Crashcourse"></img>
-      </Link>
-      <StyledH1>Crashcourse</StyledH1>
-    </StyledLogo>
+        <StyledH1>Crashcourse</StyledH1>
+      </StyledLogo>
+    </Link>
   );
 }
