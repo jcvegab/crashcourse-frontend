0624795abf75c695d64e4b23c09816c8be9d2e7d
Joseph Vega <jcvb0206@gmail.com>
2021-02-27 19:08:31 -0500

    Set test backend Django with GraphQL

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import Head from 'next/head';
 import Header from '../components/layouts/Header';
 import Main from '../components/layouts/Main';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8000',
+  cache: new InMemoryCache(),
+});
 
 export default function Home() {
   return (
